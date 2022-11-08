85c0ae5fd1052cbb41eb824499aac77f3d62a662
Kenneth Chan <kennethchan@Kenneths-MacBook-Pro.local>
2022-11-08 18:44:56 +0800

    dummy data

--- sql/data.sql
+++ sql/data.sql
@@ -1,9 +1,329 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  interest VARCHAR(255),
-  gender VARCHAR(255),
-  age INT
-)
+  subscription BOOLEAN,
+  birthday DATE,
+  mobile varchar(255)
+);
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    1,
+    'pfaccini0@is.gd',
+    'tFQQYuCvogS4',
+    true,
+    '2/16/2005',
+    '259 774 6529'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    2,
+    'dwathall1@cam.ac.uk',
+    'sDZSkM33Sp',
+    true,
+    '10/3/1959',
+    '402 875 5345'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    3,
+    'bmosby2@privacy.gov.au',
+    '1o2zBfYb5',
+    false,
+    '8/23/1993',
+    '823 967 4467'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    4,
+    'bcowans3@w3.org',
+    'aoXe9M0gl',
+    false,
+    '11/15/1963',
+    '153 919 3377'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    5,
+    'tburchell4@acquirethisname.com',
+    'nf3yjMH',
+    false,
+    '6/14/2009',
+    '782 179 5766'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    6,
+    'blighterness5@furl.net',
+    'cpOCNcaje0I',
+    true,
+    '1/17/1968',
+    '685 454 6380'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    7,
+    'mnesby6@360.cn',
+    'vD77MD1FWj',
+    false,
+    '2/1/1964',
+    '282 703 7451'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    8,
+    'bobington7@arizona.edu',
+    'ia8JAQ',
+    true,
+    '4/6/1964',
+    '779 946 4501'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    9,
+    'claffling8@tinyurl.com',
+    'Cb0Frsuj5Q7',
+    true,
+    '3/27/2001',
+    '366 394 8688'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    10,
+    'cgosenell9@trellian.com',
+    '1b7C2jJHw',
+    true,
+    '9/20/1977',
+    '359 449 6577'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    11,
+    'anarramora@vk.com',
+    'JWzCevdG',
+    true,
+    '10/2/1987',
+    '852 636 6393'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    12,
+    'dkirvinb@twitpic.com',
+    '0ToQBWZ',
+    true,
+    '8/22/2014',
+    '486 788 5435'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    13,
+    'kswinburnec@blinklist.com',
+    'g3V2u18EQ6YB',
+    false,
+    '8/20/1990',
+    '364 583 3491'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    14,
+    'cdonawayd@macromedia.com',
+    'x1XEt362',
+    true,
+    '11/23/2006',
+    '112 460 0631'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    15,
+    'bbayname@rediff.com',
+    '3jELoH',
+    true,
+    '6/4/1965',
+    '309 140 1286'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    16,
+    'smartinaf@intel.com',
+    'n1dDAK',
+    true,
+    '7/4/1952',
+    '451 784 1275'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    17,
+    'mdawdryg@e-recht24.de',
+    '4qFzuyArdzCy',
+    true,
+    '11/22/2017',
+    '192 667 0252'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    18,
+    'vkitcath@surveymonkey.com',
+    'GLeQE0psJE8',
+    true,
+    '8/3/2000',
+    '471 108 7085'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    19,
+    'kpeacopi@blogtalkradio.com',
+    'YQCJK8It6bEq',
+    true,
+    '5/21/1975',
+    '627 275 3795'
+  );
+insert into users (
+    id,
+    email,
+    password,
+    subscription,
+    birthday,
+    mobile
+  )
+values (
+    20,
+    'chavickj@booking.com',
+    'rgmcKyQMn',
+    false,
+    '8/18/1956',
+    '622 365 2901'
+  );
