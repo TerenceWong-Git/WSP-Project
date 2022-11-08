4bfce88961999a9aff73f490adf0e7e3abeaef96
Kenneth Chan <kennethchan@Kenneths-MacBook-Pro.local>
2022-11-08 18:25:25 +0800

    sql folder

--- routers/forumRoute.ts
+++ routers/forumRoute.ts
@@ -1,16 +1,15 @@
 import express from "express";
 import { client } from "../app";
+import { Post } from "../models";
 
-client;
 export const forumRoutes = express.Router();
 forumRoutes.get("/", getForumRoute);
 forumRoutes.post("/", postForumRoute);
 forumRoutes.put("/:fid", putForumRoute);
 forumRoutes.delete("/:fid", deleteForumRoute);
 
-function getForumRoute(req: express.Request, res: express.Response, next: express.NextFunction) {
-  const { title, content, userId, timeStamp } = req.body;
-  title;
+async function getForumRoute(req: express.Request, res: express.Response, next: express.NextFunction) {
+  const data = await client.query<Post>(/* sql */ `SELECT * FROM posts`);
 }
 
 function postForumRoute(req: express.Request, res: express.Response, next: express.NextFunction) {}
