ec356fe1a10af4a330b632a16e015aabdfc799bd
Eric Lam <chungfunlaam44@gmail.com>
2022-11-09 16:28:28 +0800

    register form finish

--- public/register.js
+++ public/register.js
@@ -1,17 +1,3 @@
-for (let i=1890; i<=2022; i++){    //choice of birth year
-document.querySelector("#year").innerHTML+=`
-<option value="" class="yearValue">${i}</option>
-`}
 
-for (let i=1; i<=12; i++){     //choice of birth month
-    document.querySelector("#month").innerHTML+=`
-    <option value="" class="monthValue">${i}</option>
-`}
 
-for (let i=1; i<=31; i++){     //choice of birth date
-    document.querySelector("#date").innerHTML+=`
-    <option value="" class="dateValue">${i}</option>
-`}
 
-// let month=document.querySelector()
-// if ()
