04afa760f780deb897401b703a77b1d75ca86be5
Kenneth Chan <kennethchan@Kenneths-MacBook-Pro.local>
2022-11-14 15:01:07 +0800

    added new table

--- sql/userTable.sql
+++ sql/userTable.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS products_rates_map;
+DROP TABLE IF EXISTS purchase_record_products_map;
+DROP TABLE IF EXISTS shopping_cart;
+DROP TABLE IF EXISTS purchase_record;
+DROP TABLE IF EXISTS rates;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS origin;
+DROP TABLE IF EXISTS brands;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -13,16 +22,15 @@ CREATE TABLE brands (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   decription VARCHAR(255)
-) DROP TABLE IF EXISTS orderPerTime;
-CREATE TABLE orderPerTime (
+);
+CREATE TABLE origin (
   id SERIAL PRIMARY KEY,
-  total_price decimal NOT NULL,
-  create_date date NOT NULL,
-  delivery_status int NOT NULL,
-  users_id integer,
-  comment VARCHAR(255),
-  FOREIGN KEY (users_id) REFERENCES users(id)
-) DROP TABLE IF EXISTS products;
+  name VARCHAR(255) NOT NULL
+);
+CREATE TABLE category (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -38,23 +46,36 @@ CREATE TABLE products (
   FOREIGN KEY (origin_id) REFERENCES origin(id),
   FOREIGN KEY (brands_id) REFERENCES brands(id),
   FOREIGN KEY (category_id) REFERENCES category(id)
-) DROP TABLE IF EXISTS origin;
-CREATE TABLE origin (
+);
+CREATE TABLE shopping_cart (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
-) DROP TABLE IF EXISTS category;
-CREATE TABLE category (
+  total_price decimal NOT NULL,
+  create_date date NOT NULL,
+  quantity int NOT NULL,
+  users_id integer,
+  products_id int,
+  FOREIGN KEY (users_id) REFERENCES users(id),
+  FOREIGN KEY (products_id) REFERENCES products(id)
+);
+CREATE TABLE purchase_record (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
-) DROP TABLE IF EXISTS rates;
-CREATE TABLE rates (id SERIAL PRIMARY KEY, score int NOT NULL) DROP TABLE IF EXISTS orderPerTime_products_map;
-CREATE TABLE orderPerTime_products_map (
+  total_price decimal NOT NULL,
+  create_date date NOT NULL,
+  delivery_status VARCHAR(255) NOT NULL,
+  comment VARCHAR(255),
+  products_id int,
+  users_id integer,
+  FOREIGN KEY (products_id) REFERENCES products(id),
+  FOREIGN KEY (users_id) REFERENCES users(id)
+);
+CREATE TABLE rates (id SERIAL PRIMARY KEY, score int NOT NULL);
+CREATE TABLE purchase_record_products_map (
   id SERIAL PRIMARY KEY,
-  orderPerTime_id integer,
+  purchase_record_id integer,
   product_id INTEGER,
-  FOREIGN KEY (orderPerTime_id) REFERENCES orderPerTime(id),
-  FOREIGN KEY (product_id) REFERENCES product(id),
-) DROP TABLE IF EXISTS products_rates_map;
+  FOREIGN KEY (purchase_record_id) REFERENCES purchase_record(id),
+  FOREIGN KEY (product_id) REFERENCES products(id)
+);
 CREATE TABLE products_rates_map (
   id SERIAL PRIMARY KEY,
   products_id integer,
@@ -81,7 +102,7 @@ VALUES (
     '1998-10-17',
     92153298
   ),
-(
+  (
     'abc',
     'abc@gmail.com',
     '2345',
@@ -89,7 +110,7 @@ VALUES (
     '2000-10-18',
     12345678
   ),
-(
+  (
     'bcd',
     'bcd@gmail.com',
     '3456',
@@ -102,8 +123,8 @@ SELECT *
 FROM brands;
 INSERT INTO brands (name, decription)
 VALUES ('ichiran', 'since 1990'),
-('NISSIN', 'since 1800'),
-('NONG-SHIM_SHIN', 'since 1700')
+  ('NISSIN', 'since 1800'),
+  ('NONG-SHIM_SHIN', 'since 1700')
   /* ................. build orderPerTime table .................................... */
 SELECT *
 FROM orderPerTime;
@@ -162,7 +183,7 @@ VALUES (
     1,
     1
   ),
-(
+  (
     'NISSIN_NOODLE_SEAFOOD noodles',
     'NISSHIN_NOODLE_SEAFOOD.jpg',
     'a BAD noodles',
@@ -174,7 +195,7 @@ VALUES (
     2,
     1
   ),
-(
+  (
     'NONG_SHIM_SHIN_RAMEN',
     'NONG_SHIM_SHIN_RAMEN.jpg',
     'a PERFECT noodles',
