de709b3c0bb521f91a7e76ddfea9246d8117c02a
Kenneth Chan <kennethchan@Kenneths-MacBook-Pro.local>
2022-11-15 18:20:46 +0800

    profile update

--- routers/profileRoute.ts
+++ routers/profileRoute.ts
@@ -17,11 +17,12 @@ async function getProfile(req: Request, res: Response) {
 
 async function putProfile(req: Request, res: Response) {
   if (req.session.user) {
-    const { username, mobile, birthday, subsrciption /* , password */ } = req.body;
+    const { username, mobile, birthday, subscription /* , password */ } = req.body;
     const userId = req.session.user.id;
     // const userId = 1;
     console.log("hi!!!!");
-    await client.query(/* sql */ `UPDATE users SET username = $1, mobile = $2, birthday = $3, subscription = $4 WHERE id = ${userId}`, [username, mobile, birthday, subsrciption]);
+    await client.query(/* sql */ `UPDATE users SET username = $1, mobile = $2, birthday = $3, subscription = $4 WHERE id = ${userId}`, [username, mobile, birthday, subscription]);
+    req.session.user.username = username;
     res.status(200).json({ message: "edited successfully" });
   }
 }
